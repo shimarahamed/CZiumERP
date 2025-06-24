1a6cf31163f4d0bda8152b0173d6aa60fb7e9cd1
Shimar Ahamed <shimarahamed2012@gmail.com>
2025-06-24 15:09:40 +0000

    there's no collapsable button in desktop

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -272,7 +272,7 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-7 w-7 md:flex", className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
