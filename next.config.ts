24576bb74b42c3cf86757ffb33681996a17331c6
Shimar Ahamed <shimarahamed2012@gmail.com>
2025-09-21 08:58:05 +0000

    Debugger attached.
    > nextn@0.1.0 export
    > next export
    
    Debugger attache

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -17,6 +18,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+    unoptimized: true,
   },
 };
 
