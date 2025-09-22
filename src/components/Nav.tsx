11613efc85f8eb5dfc0489527cfada0bdf9a8707
Shimar Ahamed <shimarahamed2012@gmail.com>
2025-09-22 09:33:35 +0000

    Try fixing this error: `Runtime Error: Error: Module [project]/node_modu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,7 +10,7 @@ import {
   Building2, History, Settings, Undo2, ShoppingCart, UserCog, Store, ClipboardList, 
   Archive, Clock, CalendarPlus, Banknote, UserRoundCog, BookCopy, Target, Landmark as LandmarkIcon, 
   UserPlus, Star, ClipboardCheck, Megaphone, Briefcase, LifeBuoy, Truck, Map, ChevronDown, Wrench
-} from '@/components/icons';
+} from 'lucide-react';
 import Link from 'next/link';
 import { useAppContext } from '@/context/AppContext';
 import type { Role, Module } from '@/types';
